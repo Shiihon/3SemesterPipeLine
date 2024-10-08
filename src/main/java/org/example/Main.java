27ba721282017041d7312d6f3189ccd1142ff062
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-10-08 14:23:28 +0200

    commit 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.example.config.HibernateConfig;
-
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+        System.out.println("Hello world");
     }
 }
